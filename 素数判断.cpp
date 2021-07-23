ce3fe3b337626f51b96377a973c55fb2442a517a
NTYYDS <87724806+NTYYDS@users.noreply.github.com>
2021-07-23 13:23:54 +0200

    C语言 素数判断
    试除法判断素数

---
+++ 素数判断.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h> 
+#include<math.h> 
+
+int nombrepremier(int n){
+	int count;
+	for(int i=2; i<=sqrt(n); i++){
+		if(n%i == 0){
+			count++;
+			printf("%d不是素数", n);
+			break;
+		}
+	}
+	if(count == 0){
+		printf("%d是素数",n);
+	}
+	return 0;
+}
+
+int main (){
+	int m, n;
+	printf("请输入m的值："); 
+	scanf("%d", &m);
+	nombrepremier(m);
+	return 0;
+}
+
